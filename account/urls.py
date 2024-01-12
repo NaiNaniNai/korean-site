4082c1783792e30b51a7417be80958763d776465
NaiNaniNai <nail02-02@mail.ru>
2024-01-12 22:48:02 +0300

    Add singin and logout view in account app

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 
-urlpatterns = []
+from account import views
+
+urlpatterns = [
+    path("", views.TestView.as_view(), name="test"),
+    path("singin/", views.SinginView.as_view(), name="singin"),
+    path("logout/", views.logout_view, name="logout"),
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.views import View
+from django.urls import reverse
 
-# Create your views here.
+
+class TestView(View):
+    def get(self, request):
+        return render(request, "account.html")
+
+
+class SinginView(View):
+    """Login in site"""
+
+    def get(self, request):
+        return render(request, "singin.html")
+
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(reverse("test"))
+        else:
+            return JsonResponse(
+                {
+                    "Error": "Ошибка или в имени или в пароле",
+                }
+            )
+
+
+def logout_view(request):
+    """Logout from site"""
+
+    logout(request)
+    return redirect(reverse("test"))
