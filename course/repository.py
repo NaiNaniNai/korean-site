70f658257ac859d0281efe983407329567d81fee
NaiNaniNai <nail02-02@mail.ru>
2024-03-11 20:30:19 +0300

    Refactor PassingLesson View

--- course/repository.py
+++ course/repository.py
@@ -7,6 +7,11 @@ from course.models import (
     CourseUser,
     LessonUser,
     PartOfLessonUser,
+    PartOfLesson,
+    ExerciseUser,
+    Exercise,
+    Answer,
+    ModuleUser,
 )
 from django.contrib.auth.models import User
 
@@ -20,8 +25,12 @@ class CourseRepository:
 
     @staticmethod
     def get_by_slug(slug: str) -> Course:
-        return Course.objects.filter(slug=slug).prefetch_related(
-            "modules", "modules__lessons", "modules__lessons__parts_of_lesson"
+        return (
+            Course.objects.filter(slug=slug)
+            .prefetch_related(
+                "modules", "modules__lessons", "modules__lessons__parts_of_lesson"
+            )
+            .first()
         )
 
     @staticmethod
@@ -60,3 +69,159 @@ class CourseRepository:
         user: User, lesson: Lesson
     ) -> QuerySet[PartOfLessonUser]:
         return PartOfLessonUser.objects.filter(user=user, part_of_lesson__lesson=lesson)
+
+    @staticmethod
+    def update_user_course(user: User, course: Course) -> None:
+        CourseUser.objects.filter(user=user, course=course).update(is_completed=True)
+
+
+class ModuleRepository:
+    """Class for interacting with the module model"""
+
+    @staticmethod
+    def get_by_slug(slug: str) -> Module:
+        return Module.objects.filter(slug=slug).first()
+
+    @staticmethod
+    def check_user_module(user: User, module: Module) -> bool:
+        if not ModuleUser.objects.filter(user=user, module=module):
+            return False
+        return True
+
+    @staticmethod
+    def create_user_module(user: User, module: Module) -> None:
+        ModuleUser.objects.create(user=user, module=module)
+
+    @staticmethod
+    def update_user_module(user: User, module: Module) -> None:
+        ModuleUser.objects.filter(user=user, module=module).update(is_completed=True)
+
+    @staticmethod
+    def get_user_completed_module(user: User, course: Course) -> QuerySet[ModuleUser]:
+        return ModuleUser.objects.filter(
+            user=user, module__course=course, is_completed=True
+        )
+
+
+class LessonRepository:
+    """Class for interacting with the lesson model"""
+
+    @staticmethod
+    def get_by_slug(slug: str) -> Lesson:
+        return Lesson.objects.filter(slug=slug).first()
+
+    @staticmethod
+    def get_by_module(module: Module) -> Lesson:
+        return Lesson.objects.filter(module=module).first()
+
+    @staticmethod
+    def check_user_lesson(user: User, lesson: Lesson) -> bool:
+        if not LessonUser.objects.filter(user=user, lesson=lesson):
+            return False
+        return True
+
+    @staticmethod
+    def create_user_lesson(user: User, lesson: Lesson) -> None:
+        LessonUser.objects.create(user=user, lesson=lesson)
+
+    @staticmethod
+    def update_user_lesson(user: User, lesson: Lesson) -> None:
+        LessonUser.objects.filter(user=user, lesson=lesson).update(is_completed=True)
+
+    @staticmethod
+    def get_user_completed_lesson(user: User, module: Module) -> QuerySet[LessonUser]:
+        return LessonUser.objects.filter(
+            user=user, lesson__module=module, is_completed=True
+        )
+
+
+class PartOfLessonRepository:
+    """Class for interacting with the part of lesson model"""
+
+    @staticmethod
+    def get_part_of_lesson_by_slug(lesson: Lesson, slug: str) -> PartOfLesson:
+        return PartOfLesson.objects.filter(lesson=lesson, slug=slug).first()
+
+    @staticmethod
+    def get_by_exercise(lesson: Lesson, exercise: Exercise) -> PartOfLesson:
+        return PartOfLesson.objects.filter(
+            lesson=lesson, slug=exercise.part_of_lesson.slug
+        ).first()
+
+    @staticmethod
+    def check_user_part_of_lesson(
+        user: PartOfLessonUser, part_of_lesson: PartOfLessonUser
+    ) -> bool:
+        if not PartOfLessonUser.objects.filter(
+            user=user, part_of_lesson=part_of_lesson
+        ):
+            return False
+        return True
+
+    @staticmethod
+    def create_user_part_of_lesson(user: User, part_of_lesson: PartOfLesson) -> None:
+        PartOfLessonUser.objects.create(user=user, part_of_lesson=part_of_lesson)
+
+    @staticmethod
+    def update_user_part_of_lesson(user: User, part_of_lesson: PartOfLesson) -> None:
+        PartOfLessonUser.objects.filter(
+            user=user, part_of_lesson=part_of_lesson
+        ).update(is_completed=True)
+
+    @staticmethod
+    def get_user_completed_part_of_lesson(
+        user: User, lesson: Lesson
+    ) -> QuerySet[PartOfLessonUser]:
+        return PartOfLessonUser.objects.filter(
+            user=user, part_of_lesson__lesson=lesson, is_completed=True
+        )
+
+
+class ExerciseRepository:
+    """Class for interacting with the exercise model"""
+
+    @staticmethod
+    def get_by_id(id: int) -> Exercise:
+        return Exercise.objects.filter(id=id).first()
+
+    @staticmethod
+    def get_user_completed_exercises(
+        user: User, part_of_lesson: PartOfLessonUser
+    ) -> QuerySet[ExerciseUser]:
+        return ExerciseUser.objects.filter(
+            user=user, exercise__part_of_lesson=part_of_lesson, is_completed=True
+        )
+
+    @staticmethod
+    def check_completed(user: User, exercise: Exercise) -> bool:
+        is_completed = ExerciseUser.objects.filter(
+            user=user, exercise=exercise, is_completed=True
+        )
+        if not is_completed:
+            return False
+        return True
+
+    @staticmethod
+    def update_of_create(user: User, exercise: Exercise) -> None:
+        ExerciseUser.objects.update_or_create(
+            user=user,
+            exercise=exercise,
+            defaults={
+                "is_completed": True,
+            },
+        )
+
+
+class AnswerRepository:
+    """Class for intracting with the answer model"""
+
+    @staticmethod
+    def get_by_id(id: int) -> Answer:
+        return Answer.objects.filter(id=id).first()
+
+    @staticmethod
+    def check_correct(answer: Answer) -> bool:
+        is_correct = answer.is_correct
+        if not is_correct:
+            return False
+        return True
--- course/views.py
+++ course/views.py
@@ -1,22 +1,13 @@
-from django.contrib import messages
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
-from .models import (
-    Course,
-    Module,
-    CourseUser,
-    LessonUser,
-    ExerciseUser,
-    Lesson,
-    PartOfLessonUser,
-    PartOfLesson,
-    Answer,
-    Exercise,
-    ModuleUser,
+from .service import (
+    CourseListService,
+    CourseDetailService,
+    PassingCourseService,
+    PassingLessonService,
 )
-from .service import CourseListService, CourseDetailService, PassingCourseService
 
 
 class CourseListView(View):
@@ -50,161 +41,11 @@ class PassingLessonView(View):
     """Passing the lesson"""
 
     def get(self, request, course_slug, module_slug, lesson_slug):
-        course = Course.objects.prefetch_related("modules__lessons").get(
-            slug=course_slug
-        )
-        module = Module.objects.filter(slug=module_slug).first()
-        user = request.user
-        user_course = None
-        if user.id:
-            user_course = CourseUser.objects.filter(course=course, user=user).first()
-        is_available = False
-        if user_course:
-            is_available = user_course.is_available
-        lesson = Lesson.objects.filter(
-            module__course=course, module__slug=module_slug, slug=lesson_slug
-        ).first()
-        theory_part = PartOfLesson.objects.filter(lesson=lesson, slug="theory").first()
-        theory_completed_exercises = ExerciseUser.objects.filter(
-            user=user, exercise__part_of_lesson=theory_part, is_completed=True
-        )
-        practical_part = PartOfLesson.objects.filter(
-            lesson=lesson, slug="practical"
-        ).first()
-        practical_part_completed_exercises = ExerciseUser.objects.filter(
-            user=user, exercise__part_of_lesson=practical_part, is_completed=True
-        ).values_list("exercise", flat=True)
-        homework_part = PartOfLesson.objects.filter(
-            lesson=lesson, slug="homework"
-        ).first()
-        homework_part_completed_exercises = ExerciseUser.objects.filter(
-            user=user, exercise__part_of_lesson=homework_part, is_completed=True
-        )
-
-        context = {
-            "course": course,
-            "module": module,
-            "lesson": lesson,
-            "is_available": is_available,
-            "theory_part": theory_part,
-            "practical_part": practical_part,
-            "homework_part": homework_part,
-            "theory_completed_exercises": theory_completed_exercises,
-            "practical_part_completed_exercises": practical_part_completed_exercises,
-            "homework_part_completed_exercises": homework_part_completed_exercises,
-        }
+        service = PassingLessonService(request)
+        context = service.get(course_slug, module_slug, lesson_slug)
         return render(request, "passing_lesson.html", context)
 
     def post(self, request, course_slug, module_slug, lesson_slug):
-        user = request.user
-        answer_id = request.POST.get("answer_id")
-        exercise_id = request.POST.get("exercises_id")
-        exercise = Exercise.objects.filter(id=exercise_id)
-        completed_exercise = ExerciseUser.objects.filter(
-            exercise_id=exercise_id, is_completed=True
-        )
-
-        if answer_id:
-            answer = Answer.objects.filter(id=answer_id).first()
-            if not answer.is_correct:
-                messages.error(request, "Неправильный ответ")
-                return JsonResponse({"fail": True})
-
-        if list(exercise.values_list("id")) == list(
-            completed_exercise.values_list("exercise")
-        ):
-            return JsonResponse({"success": False})
-
-        ExerciseUser.objects.update_or_create(
-            user=user,
-            exercise=exercise.first(),
-            defaults={
-                "is_completed": True,
-            },
-        )
-        self.passing_course(user, course_slug, module_slug, exercise.first())
-
-        return JsonResponse({"success": True})
-
-    def passing_course(self, user, course_slug, module_slug, exercise):
-        course = Course.objects.filter(slug=course_slug).first()
-
-        module = Module.objects.filter(slug=module_slug).first()
-        user_module = ModuleUser.objects.filter(user=user, module=module).first()
-
-        if not user_module:
-            ModuleUser.objects.create(
-                user=user,
-                module=module,
-            )
-
-        lesson = Lesson.objects.filter(module=module).first()
-        user_lesson = LessonUser.objects.filter(user=user, lesson=lesson).first()
-
-        if not user_lesson:
-            LessonUser.objects.create(
-                user=user,
-                lesson=lesson,
-            )
-
-        part_of_lesson = PartOfLesson.objects.filter(
-            lesson=lesson, slug=exercise.part_of_lesson.slug
-        ).first()
-        part_of_lesson_user = PartOfLessonUser.objects.filter(
-            user=user, part_of_lesson=part_of_lesson
-        ).first()
-
-        if not part_of_lesson_user:
-            PartOfLessonUser.objects.create(user=user, part_of_lesson=part_of_lesson)
-
-        exercises_part_of_lesson = list(part_of_lesson.exercises.values_list("id"))
-        exercises_part_of_lesson_user = list(
-            ExerciseUser.objects.filter(
-                user=user, exercise__part_of_lesson=part_of_lesson, is_completed=True
-            )
-            .order_by("exercise")
-            .values_list("exercise")
-        )
-        if exercises_part_of_lesson == exercises_part_of_lesson_user:
-            PartOfLessonUser.objects.filter(
-                user=user, part_of_lesson=part_of_lesson
-            ).update(is_completed=True)
-
-        parts_of_lesson = list(lesson.parts_of_lesson.values_list("id"))
-        user_parts_of_lesson = list(
-            PartOfLessonUser.objects.filter(
-                user=user, part_of_lesson__lesson=lesson, is_completed=True
-            )
-            .order_by("part_of_lesson")
-            .values_list("part_of_lesson")
-        )
-        if parts_of_lesson == user_parts_of_lesson:
-            LessonUser.objects.filter(user=user, lesson=lesson).update(
-                is_completed=True
-            )
-
-        lessons_of_module = list(module.lessons.values_list("id"))
-        user_lessons_of_module = list(
-            LessonUser.objects.filter(
-                user=user, lesson__module=module, is_completed=True
-            )
-            .order_by("lesson")
-            .values_list("lesson")
-        )
-        if lessons_of_module == user_lessons_of_module:
-            ModuleUser.objects.filter(user=user, module=module).update(
-                is_completed=True
-            )
-
-        modules_of_course = list(course.modules.values_list("id"))
-        user_modules_of_course = list(
-            ModuleUser.objects.filter(
-                user=user, module__course=course, is_completed=True
-            )
-            .order_by("module")
-            .values_list("module")
-        )
-        if modules_of_course == user_modules_of_course:
-            CourseUser.objects.filter(user=user, course=course).update(
-                is_completed=True
-            )
+        service = PassingLessonService(request)
+        context = service.post(course_slug, module_slug, lesson_slug)
+        return JsonResponse(context)
