b97991730db1803e10da1505a503d5ccb51db39f
NaiNaniNai <nail02-02@mail.ru>
2024-03-12 23:01:06 +0300

    Add celery and redis

--- project_root/settings.py
+++ project_root/settings.py
@@ -267,3 +267,13 @@ CKEDITOR_CONFIGS = {
         ),
     }
 }
+
+
+REDIS_HOST = env("REDIS_HOST")
+REDIS_PORT = env("REDIS_PORT")
+CELERY_BROKER_URL = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
+CELERY_BROKER_TRANSPORT_OPTIONS = {"visibility_timeout": 3600}
+CELERY_RESULT_BACKEND = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
